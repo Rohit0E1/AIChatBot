083ae6ca9fdbc82eedc104e93787da588f014b9d
Rohit Kumar <rsahoo@gravityer.com>
2026-01-28 12:40:10 +0530

    feat: change model

--- app/api/completion/route.js
+++ app/api/completion/route.js
@@ -48,7 +48,7 @@ export const POST = async (req, res) => {
         console.log("-------------------------", systemPrompt);
         console.log("links----------------", links);
         const { text, toolCalls } = await generateText({
-            model: google("gemini-2.5-flash-lite"),
+            model: google("gemini-3-flash-preview"),
             system: systemPrompt,
             prompt: `${prompt}`,
             tools: {
